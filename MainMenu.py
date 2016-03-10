3dc9eeabead5f2dbaf305a06ea7db69a5bb5a85d
freshollie <freshsignups@gmail.com>
2016-03-10 14:29:36 +0000

    Added main menu

---
+++ MainMenu.py
@@ -0,0 +1,44 @@
+import sys
+from tkinter import *
+
+import Settings
+import GuiShoppingList
+
+
+root = Tk()
+
+class MainMenu:
+    def __init__(self, root):
+        self.root = root
+        self.MainMenuText = Label(self.root, text='Main Menu', font=("Helvetica", 28), bg='#E0EBFF')
+        self.MainMenuText.pack(pady=20)
+
+        Button(self.root, text='Run Simulation', font=("Helvetica", 16), bg='white', height=1, width=13, command = runSimulation).pack(pady=80)
+        Button(self.root, text='Settings', font=("Helvetica", 16), bg='white', height=1, width=13, command = runSettings).pack(side=TOP)
+
+        
+        
+
+def main():
+    root.resizable(width=FALSE, height=FALSE)
+    #Disable resizing
+    gui = MainMenu(root)
+    root.geometry('{}x{}'.format(700, 500))
+    #Set window size
+    BackgroundColour = '#E0EBFF'
+    root.configure(bg=BackgroundColour)
+    #Changes background colour to off white
+    root.wm_title("Virtual Robot Bargain Hunt")
+    #Changes window title
+    root.mainloop()
+
+
+def runSimulation():
+    print(Settings.counter.get())
+    GuiShoppingList.main()
+
+def runSettings():
+    Settings.main(Toplevel())
+
+
+main()
